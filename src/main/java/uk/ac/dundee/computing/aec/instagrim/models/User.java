818c2a3debc49d4c318264624cfbca4a280956f2
blairey <blaireemilymcleod@gmail.com>
2015-10-16 10:49:29 +0100

    net beans and cassandra

---
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/models/User.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uk.ac.dundee.computing.aec.instagrim.models;
+
+import com.datastax.driver.core.BoundStatement;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import uk.ac.dundee.computing.aec.instagrim.lib.AeSimpleSHA1;
+import uk.ac.dundee.computing.aec.instagrim.stores.Pic;
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ *
+ * @author Administrator
+ */
+public class User {
+    Cluster cluster;
+    public User(){
+        
+    }
+    
+    public boolean RegisterUser(String username, String Password, String first_name , String last_name, String email){
+        AeSimpleSHA1 sha1handler=  new AeSimpleSHA1();
+        String EncodedPassword=null;
+        try {
+            EncodedPassword= sha1handler.SHA1(Password);
+        }catch (UnsupportedEncodingException | NoSuchAlgorithmException et){
+            System.out.println("Can't check your password");
+            return false;
+        }
+        Session session = cluster.connect("instagrim");
+        PreparedStatement ps = session.prepare("INSERT INTO instagrim.userprofiles (login, first_name, last_name, password, email) VALUES(?,?,?,?, ?);");
+        
+        BoundStatement boundStatement = ps.bind();
+        
+        boundStatement.setString("login", username);
+        boundStatement.setString("password", EncodedPassword);
+        boundStatement.setString("first_name", first_name);
+        boundStatement.setString("last_name", last_name);
+        boundStatement.setString("email", email);
+        
+       session.execute(boundStatement);
+        //We are assuming this always works.  Also a transaction would be good here 
+    
+        return true;
+    }
+    
+    public boolean IsValidUser(String username, String Password){
+        AeSimpleSHA1 sha1handler=  new AeSimpleSHA1();
+        String EncodedPassword=null;
+        try {
+            EncodedPassword= sha1handler.SHA1(Password);
+        }catch (UnsupportedEncodingException | NoSuchAlgorithmException et){
+            System.out.println("Can't check your password");
+            return false;
+        }
+        Session session = cluster.connect("instagrim");
+        PreparedStatement ps = session.prepare("select password from userprofiles where login =?");
+        ResultSet rs = null;
+        BoundStatement boundStatement = new BoundStatement(ps);
+        rs = session.execute( // this is where the query is executed
+                boundStatement.bind( // here you are binding the 'boundStatement'
+                        username));
+        if (rs.isExhausted()) {
+            System.out.println("No Images returned");
+            return false;
+        } else {
+            for (Row row : rs) {
+               
+                String StoredPass = row.getString("password");
+                if (StoredPass.compareTo(EncodedPassword) == 0)
+                    return true;
+            }
+        }
+   
+    
+    return false;  
+    }
+       public void setCluster(Cluster cluster) {
+        this.cluster = cluster;
+    }
+
+    
+}
